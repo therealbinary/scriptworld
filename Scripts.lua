57f6c195553679b1f11575b780716138dc001b8f
binary <126812477+therealbinary@users.noreply.github.com>
2023-04-16 22:25:26 +0200

    Update Scripts.lua

--- Scripts.lua
+++ Scripts.lua
@@ -1,1 +1,19 @@
-
+local Scripts = {
+    [11746859781] = {
+        {
+            Name = "Casual GUI",
+            Information = "Clean Gui, Bunch of features, a bit buggy though...",
+            Loadstring = "https://raw.githubusercontent.com/Tumppi66/Bubble-Gum-Clicker/main/3.0.6%20OBFUSCATED.lua",
+            Key = false, 
+            Date = "4/16/2023";
+        },
+        {
+            Name = {"Binary", "Binary // Personal Favorite"},
+            Information = "Very nice script, has all needed features, a bit outdated",
+            Loadstring = "https://raw.githubusercontent.com/therealbinary/BinaryGames/main/11746859781.lua",
+            Key = false,
+            Date = "4/16/2023";
+        }
+    }
+}
+return Scripts
